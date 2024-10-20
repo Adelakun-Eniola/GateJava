8622a450927b4eb6d3879c184759daff5de7d906
Adelakun-Eniola <145209142+Adelakun-Eniola@users.noreply.github.com>
2024-10-21 00:51:14 +0100

    projects1

---
+++ PersonalityTestTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class PersonalityTestTest{
+	@Test
+	/*public void testThatUserInputWorks(String userName){
+		PersonalityTest.userName();
+		String name = PersonalityTest.userName();
+		assertEquals( name, userName);
+	}*/
+
+	@Test
+	public void mainOperation(String[][] arr, String[] answers){
+	
+	
+	
+	}
+
+
+}
